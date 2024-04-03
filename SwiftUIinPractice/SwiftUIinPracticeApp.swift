6fd24f23894d944fe30e477c50c54590fb6a9990
Felipe Weber <felipe_weber@sicredi.com.br>
2024-04-03 05:57:52 -0300

    Initial Commit

---
+++ SwiftUIinPractice/SwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeApp.swift
+//  SwiftUIinPractice
+//
+//  Created by Felipe Weber on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
